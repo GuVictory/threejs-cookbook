ed9e86591d43ff3700c4859f383dc16b0c354a8b
Victoria <gubanova.vi10@gmail.com>
2022-03-09 17:34:15 +0000

    added basics/lesson-4

--- src/script.js
+++ src/script.js
@@ -28,5 +28,19 @@ const renderer = new THREE.WebGLRenderer({
     canvas: document.querySelector('canvas.canvas-js')
 });
 
+// Передвижение куба
+/* mesh.position.x = -1;
+mesh.position.y = -0.8;
+mesh.position.z = 0.4; */
+
+// Масштабирование куба
+mesh.scale.x = 0.5;
+mesh.scale.y = 2;
+mesh.scale.z = 0.7;
+
+// Отрисовка осей координат
+const axesHelper = new THREE.AxesHelper(3);
+scene.add(axesHelper);
+
 renderer.setSize(sizes.width, sizes.height);
 renderer.render(scene, camera);
