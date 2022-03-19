7a19ba8b3f4ff661d8a25ac09212fb411bb01f06
Victoria <gubanova.vi10@gmail.com>
2022-03-19 18:08:39 +0000

    added basics/lesson-5

--- src/script.js
+++ src/script.js
@@ -9,8 +9,9 @@ const scene = new THREE.Scene();
 // Объект
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 'purple', wireframe: true });
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+
+// const mesh = new THREE.Mesh(geometry, material);
+// scene.add(mesh);
 
 // Объект с размерами
 const sizes = {
@@ -21,6 +22,7 @@ const sizes = {
 // Камера
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
 camera.position.z = 3;
+
 scene.add(camera);
 
 // Отрисовщик
@@ -28,15 +30,42 @@ const renderer = new THREE.WebGLRenderer({
     canvas: document.querySelector('canvas.canvas-js')
 });
 
+/* 
 // Передвижение куба
-/* mesh.position.x = -1;
+mesh.position.x = -1;
 mesh.position.y = -0.8;
-mesh.position.z = 0.4; */
+mesh.position.z = 0.4;
 
 // Масштабирование куба
 mesh.scale.x = 0.5;
 mesh.scale.y = 2;
 mesh.scale.z = 0.7;
+
+mesh.rotation.x = Math.PI * 0.25;
+mesh.rotation.y = Math.PI * 0.25;
+
+camera.lookAt(new THREE.Vector3(0, - 1, 0));
+*/
+
+/**
+ * Группировка обхектов
+ */
+const group = new THREE.Group();
+group.scale.y = 0.7;
+group.rotation.x = 0.5;
+scene.add(group);
+ 
+const cube1 = new THREE.Mesh(geometry, material);
+cube1.position.x = - 1.2;
+group.add(cube1);
+
+const cube2 = new THREE.Mesh(geometry, material);
+cube2.position.x = 0;
+group.add(cube2);
+ 
+const cube3 =  new THREE.Mesh(geometry, material);
+cube3.position.x = 1.2;
+group.add(cube3);
 
 // Отрисовка осей координат
 const axesHelper = new THREE.AxesHelper(3);
