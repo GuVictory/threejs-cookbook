77721bad72c826cecfd573c2cedd8025719c43b3
Victoria <gubanova.vi10@gmail.com>
2022-03-22 17:14:07 +0000

    added source for basics/lesson-6

--- src/script.js
+++ src/script.js
@@ -36,4 +36,33 @@ const axesHelper = new THREE.AxesHelper(3);
 scene.add(axesHelper);
 
 renderer.setSize(sizes.width, sizes.height);
-renderer.render(scene, camera);
+
+const clock = new THREE.Clock();
+
+/**
+ * Анимация
+ */
+const animateLoop = () =>
+{
+    const elapsedTime = clock.getElapsedTime();
+    
+    // Вращение объекта
+    // mesh.rotation.y = elapsedTime;
+
+    // Передвижение объекта
+    // mesh.position.x = Math.cos(elapsedTime);
+    // mesh.position.y = Math.sin(elapsedTime);
+
+    // Трансформация камеры
+    camera.position.x = Math.cos(elapsedTime);
+    camera.position.y = Math.sin(elapsedTime);
+    camera.lookAt(mesh.position);
+
+    // Отрисовываем
+    renderer.render(scene, camera);
+
+    // Вызываем функцию анимации в следующем фрейме
+    window.requestAnimationFrame(animateLoop);
+};
+
+animateLoop();
